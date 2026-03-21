d973141f45748a2ccbe8eeb1871c432f38ffe5be
Avnish Kumar <Avnish.Kumar@365labs.com>
2026-03-21 14:21:17 +0530

    Update assembly informational version and cache inputs

--- src/C.Compiler/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/C.Compiler.AssemblyInfo.cs
+++ src/C.Compiler/obj/ARM64/Debug/net8.0-windows10.0.19041.0/win-arm64/C.Compiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C.Compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca0c3100824a7daf0863d03eea0ad6f55aef3af")]
 [assembly: System.Reflection.AssemblyProductAttribute("C.Compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C.Compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
